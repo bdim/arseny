782ce8b69c78d6b261c6c777ad66430253abd343
bdim <heikki@mail.ru>
2017-11-22 16:49:12 +0500

    git

--- config/db.php
+++ config/db.php
@@ -1,10 +1,5 @@
 <?php
 
 return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=heikki_arseny17',
-    'username' => 'heikki_arseny17',
-    'password' => 'xGV2ap2Z',
-    'charset' => 'utf8',
-    'tablePrefix' => 'yii2_',
+
 ];
