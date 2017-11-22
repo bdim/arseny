58ba51a497f32fee500116a4ff31dc6ed7a165c3
Дмитрий Бурдин <bdim@users.noreply.github.com>
2017-11-22 16:45:08 +0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=heikki_arseny17',
-    'username' => 'heikki_arseny17',
-    'password' => 'xGV2ap2Z',
+    'dsn' => '',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
-    'tablePrefix' => 'yii2_',
+    'tablePrefix' => '',
 ];
